84da2ca6b05f635eb832220d2efe40db2b3ad191
matthewdeanmartin@gmail.com <matthew.martin@bm.com>
2017-03-15 10:32:46 -0400

    more explanatory test

--- kata/conversation_tree/main.py
+++ kata/conversation_tree/main.py
@@ -2,14 +2,22 @@
 """
 Implement a conversation tree, e.g.
 
-User says, and gets in response one of the following.
-Hello -> (How are you?, Hello, Go Away)
-
-User now must pick next utterance from list
-How are you? -> (Me too!, I'm sick, Oh well)
-
-If the user picked, say "Me too!", the computer responds with
-Me too! -> (See you!, Oh well, That's great!)
+User says Hello, and gets in response one of the following.
+User picks from numbered items, eg. U1,U2
+U:Hello ->
+    C:How are you?
+        U1: Fine
+            C: Me too!
+                U1: What's happening today?
+                U2: Where is the library?
+        U2: I'm sick
+            C: Sorry to hear that.
+                U1: Where is a pharmacy?
+                U2: Where is the hospital?
+        U3: Oh well
+            C: You sound disinterested.
+                U1: No, at at all.
+                U2: You got that right
 
 Level 1 - data is mixed with code
 Level 2 - data is neatly segregated from code.
--- kata/count_by/main.py
+++ kata/count_by/main.py
@@ -1,6 +1,12 @@
 # coding=utf-8
 """"
 Count by n.
+
+Print to the screen numbers 1, 2, 3, but also 2, 4, 6 (counting by 2), etc.
+
+Be explicit about when the code should start and stop counting.
+
+Make memory efficient if possible.
 """
 from __future__ import print_function, unicode_literals
 
